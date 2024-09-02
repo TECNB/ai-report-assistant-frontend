3781057cd1132e26d83831735d95a1baceef3a79
TECNB <3489044730@qq.com>
2024-09-02 14:16:01 +0800

    chore(setup): 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+//注意引入
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+
+    //下面是新加入的内容
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer]
+      }
+    }
+})
