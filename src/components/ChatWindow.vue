035ddf03cfa2ce7db5c7349011f4a08907d306c4
TECNB <3489044730@qq.com>
2024-09-12 20:10:48 +0800

    fix(ChatWindow.vue):修复过渡效果失效的问题

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -169,6 +169,16 @@ const updateAICodeVisible = (newValue: boolean) => {
 </script>
 
 <style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
 .skeleton-loader {
     display: flex;
     flex-direction: column;
