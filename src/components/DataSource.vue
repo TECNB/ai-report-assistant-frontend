224067daf8987702d33128e0868d51ba89710dc2
xusupport <1102663302@qq.com>
2024-09-18 23:04:49 +0800

    fix(DataSource+DatabaseForm):修改了弹窗提示的效果

--- src/components/DataSource.vue
+++ src/components/DataSource.vue
@@ -96,6 +96,15 @@ const submitForm = () => {
 </script>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
 
 .Chart {
     width: 90%;
