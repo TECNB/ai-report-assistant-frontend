a2b9d358a118379c7e92d3ba03d42d072ea29dbb
TECNB <3489044730@qq.com>
2024-09-24 21:58:57 +0800

    feat(SidebarForm.vue):完成表格侧栏的界面

---
+++ src/constant/formContant.ts
@@ -0,0 +1,30 @@
+// src/formContant.ts
+export const formContant = [
+    {
+        source: '🌍 2023中国生态环境状况公报',
+        items: [
+            { text: '空气质量表' },
+            { text: '河流污染表' },
+            { text: '自然保护区' },
+            { text: '森林覆盖率表' },
+        ],
+    },
+    {
+        source: '🐞 Bug跟踪管理系统',
+        items: [
+            { text: 'Bug趋势表' },
+            { text: '修复进度表' },
+            { text: '测试覆盖率表' },
+            { text: '高优先级Bug表' },
+        ],
+    },
+    {
+        source: '⚡ 能源消耗统计年报',
+        items: [
+            { text: '电力消耗表' },
+            { text: '燃料使用表' },
+            { text: '水资源消耗表' },
+            { text: '可再生能源表' },
+        ],
+    }
+];
