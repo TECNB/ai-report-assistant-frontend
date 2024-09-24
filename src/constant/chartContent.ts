326429fb100d3af1d1fdcf1cfc4a72b320f22c0a
TECNB <3489044730@qq.com>
2024-09-24 22:47:04 +0800

    feat(SidebarForm.vue):完成图表侧栏的界面

---
+++ src/constant/chartContent.ts
@@ -0,0 +1,30 @@
+// src/chartContent.ts
+export const chartContent = [
+    {
+        source: '🌍 2023中国生态环境状况公报',
+        items: [
+            { text: '全国空气质量趋势图', icon: 'fa-chart-line' },
+            { text: '主要河流水质变化图', icon: 'fa-chart-simple' },
+            { text: '自然保护区分布图', icon: 'fa-earth-americas' },
+            { text: '森林覆盖率变化图', icon: 'fa-chart-radar' },
+        ],
+    },
+    {
+        source: '📊 市场分析报告 2023',
+        items: [
+            { text: '产品销售趋势图', icon: 'fa-chart-line' },
+            { text: '市场份额图', icon: 'fa-chart-pie' },
+            { text: '区域分布图', icon: 'fa-earth-americas' },
+            { text: '客户满意度图', icon: 'fa-chart-radar' },
+        ],
+    },
+    {
+        source: '🏦 金融数据分析',
+        items: [
+            { text: '股票价格波动图', icon: 'fa-chart-line' },
+            { text: '收益率曲线', icon: 'fa-chart-line' },
+            { text: '债务比率图', icon: 'fa-chart-simple' },
+            { text: '投资回报率图', icon: 'fa-chart-scatter-bubble' },
+        ],
+    },
+];
