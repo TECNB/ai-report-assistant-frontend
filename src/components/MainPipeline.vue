8f8066aa190d13a1f46b4920116d6ea3bad639b3
TECNB <3489044730@qq.com>
2024-10-03 23:02:17 +0800

    feat(MainPipeline.vue):提取准入设置按钮组件进行重构

--- src/components/MainPipeline.vue
+++ src/components/MainPipeline.vue
@@ -87,54 +87,8 @@
                     <!-- 主要部分 -->
                     <div class="w-72 h-[800px] border-r-2 border-gray-500 border-dashed translate-x-7 p-5">
                         <!-- 准入设置按钮 -->
-                        <el-popover :visible="accessSettingsVisible1" placement="bottom" :width="400">
-                            <div class="flex justify-between items-center border-b -mx-5 px-5 pb-2 ">
-                                <p class="text-lg text-black font-bold">准入设置</p>
-                                <div class="Close" @click="accessSettingsVisible1 = false">
-                                    <el-icon size="20" class="cursor-pointer">
-                                        <Close />
-                                    </el-icon>
-                                </div>
-                            </div>
-
-                            <div class="flex justify-start items-center gap-3 mt-5">
-                                <p>设置帮助</p>
-                                <el-popover :visible="accessSettingsHelpVisible1" placement="right" :width="650">
-                                    <p>自动：流水线执行时自动进入该阶段继续运行。</p>
-                                    <p>手动：流水线执行时需手动确认后才能进入该阶段继续运行。</p>
-                                    <p>时间窗：流水线阶段任务在执行窗口内时可以运行，若未在执行窗口内则无法运行该阶段任务。</p>
-                                    <template #reference>
-                                        <i class="fa-light fa-circle-question"
-                                            @click="accessSettingsHelpVisible1 = !accessSettingsHelpVisible1"></i>
-                                    </template>
-                                </el-popover>
-
-                            </div>
-                            <div class="mt-5">
-                                <el-checkbox v-model="accessSettings1" label="自动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings2" label="手动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings3" label="时间窗" class="el-checkbox-class" />
-                            </div>
-
-
-                            <div class="flex justify-center items-center gap-5 mt-10">
-                                <div class="bg-black cursor-pointer rounded-lg px-10 py-2"
-                                    @click="accessSettingsVisible1 = false">
-                                    <p class=" text-white">确定</p>
-                                </div>
-
-                                <div class="bg-white cursor-pointer rounded-lg border px-10 py-2"
-                                    @click="accessSettingsVisible1 = false">
-                                    <p class=" text-black">取消</p>
-                                </div>
-                            </div>
-                            <template #reference>
-                                <div class="w-7 h-7 absolute -left-14 top-8 bg-black rounded-full z-50 flex justify-center items-center"
-                                    @click="showAccessSettings(1)">
-                                    <i class="fa-light fa-up-to-bracket fa-rotate-90 " style="color: white;"></i>
-                                </div>
-                            </template>
-                        </el-popover>
+                        <PipelineAccessSettings />
+                        
 
                         <div class="-translate-x-5 relative">
                             <div class="container  rounded-lg">
@@ -195,54 +149,8 @@
                     <!-- 主要部分 -->
                     <div class="w-72 h-[800px] relative border-r-2 border-gray-500 border-dashed translate-x-7 p-5">
                         <!-- 准入设置按钮 -->
-                        <el-popover :visible="accessSettingsVisible2" placement="bottom" :width="400">
-                            <div class="flex justify-between items-center border-b -mx-5 px-5 pb-2 ">
-                                <p class="text-lg text-black font-bold">准入设置</p>
-                                <div class="Close" @click="accessSettingsVisible2 = false">
-                                    <el-icon size="20" class="cursor-pointer">
-                                        <Close />
-                                    </el-icon>
-                                </div>
-                            </div>
+                        <PipelineAccessSettings />
 
-                            <div class="flex justify-start items-center gap-3 mt-5">
-                                <p>设置帮助</p>
-                                <el-popover :visible="accessSettingsHelpVisible2" placement="right" :width="650">
-                                    <p>自动：流水线执行时自动进入该阶段继续运行。</p>
-                                    <p>手动：流水线执行时需手动确认后才能进入该阶段继续运行。</p>
-                                    <p>时间窗：流水线阶段任务在执行窗口内时可以运行，若未在执行窗口内则无法运行该阶段任务。</p>
-                                    <template #reference>
-                                        <i class="fa-light fa-circle-question"
-                                            @click="accessSettingsHelpVisible2 = !accessSettingsHelpVisible2"></i>
-                                    </template>
-                                </el-popover>
-
-                            </div>
-                            <div class="mt-5">
-                                <el-checkbox v-model="accessSettings1" label="自动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings2" label="手动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings3" label="时间窗" class="el-checkbox-class" />
-                            </div>
-
-
-                            <div class="flex justify-center items-center gap-5 mt-10">
-                                <div class="bg-black cursor-pointer rounded-lg px-10 py-2"
-                                    @click="accessSettingsVisible2 = false">
-                                    <p class=" text-white">确定</p>
-                                </div>
-
-                                <div class="bg-white cursor-pointer rounded-lg border px-10 py-2"
-                                    @click="accessSettingsVisible2 = false">
-                                    <p class=" text-black">取消</p>
-                                </div>
-                            </div>
-                            <template #reference>
-                                <div class="w-7 h-7 absolute -left-14 top-8 bg-black rounded-full z-50 flex justify-center items-center"
-                                    @click="showAccessSettings(2)">
-                                    <i class="fa-light fa-up-to-bracket fa-rotate-90 " style="color: white;"></i>
-                                </div>
-                            </template>
-                        </el-popover>
                         <div class="-translate-x-5">
                             <div
                                 class="relative flex flex-col bg-white p-3 border-4 border-transparent cursor-pointer rounded-xl shadow-lg hover:border-gray-300">
@@ -335,54 +243,8 @@
                     <!-- 主要部分 -->
                     <div class="w-72 h-[800px] border-r-2 border-gray-500 border-dashed translate-x-7 p-5">
                         <!-- 准入设置按钮 -->
-                        <el-popover :visible="accessSettingsVisible3" placement="bottom" :width="400">
-                            <div class="flex justify-between items-center border-b -mx-5 px-5 pb-2 ">
-                                <p class="text-lg text-black font-bold">准入设置</p>
-                                <div class="Close" @click="accessSettingsVisible3 = false">
-                                    <el-icon size="20" class="cursor-pointer">
-                                        <Close />
-                                    </el-icon>
-                                </div>
-                            </div>
+                        <PipelineAccessSettings />
 
-                            <div class="flex justify-start items-center gap-3 mt-5">
-                                <p>设置帮助</p>
-                                <el-popover :visible="accessSettingsHelpVisible3" placement="right" :width="650">
-                                    <p>自动：流水线执行时自动进入该阶段继续运行。</p>
-                                    <p>手动：流水线执行时需手动确认后才能进入该阶段继续运行。</p>
-                                    <p>时间窗：流水线阶段任务在执行窗口内时可以运行，若未在执行窗口内则无法运行该阶段任务。</p>
-                                    <template #reference>
-                                        <i class="fa-light fa-circle-question"
-                                            @click="accessSettingsHelpVisible3 = !accessSettingsHelpVisible3"></i>
-                                    </template>
-                                </el-popover>
-
-                            </div>
-                            <div class="mt-5">
-                                <el-checkbox v-model="accessSettings1" label="自动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings2" label="手动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings3" label="时间窗" class="el-checkbox-class" />
-                            </div>
-
-
-                            <div class="flex justify-center items-center gap-5 mt-10">
-                                <div class="bg-black cursor-pointer rounded-lg px-10 py-2"
-                                    @click="accessSettingsVisible3 = false">
-                                    <p class=" text-white">确定</p>
-                                </div>
-
-                                <div class="bg-white cursor-pointer rounded-lg border px-10 py-2"
-                                    @click="accessSettingsVisible3 = false">
-                                    <p class=" text-black">取消</p>
-                                </div>
-                            </div>
-                            <template #reference>
-                                <div class="w-7 h-7 absolute -left-14 top-8 bg-black rounded-full z-50 flex justify-center items-center"
-                                    @click="showAccessSettings(3)">
-                                    <i class="fa-light fa-up-to-bracket fa-rotate-90 " style="color: white;"></i>
-                                </div>
-                            </template>
-                        </el-popover>
                         <div class="-translate-x-5">
                             <div
                                 class="relative flex flex-col bg-white p-3 border-4 border-transparent cursor-pointer rounded-xl shadow-lg hover:border-gray-300">
@@ -429,54 +291,8 @@
                     <!-- 主要部分 -->
                     <div class="w-72 h-[800px] translate-x-7 p-5">
                         <!-- 准入设置按钮 -->
-                        <el-popover :visible="accessSettingsVisible3" placement="bottom" :width="400">
-                            <div class="flex justify-between items-center border-b -mx-5 px-5 pb-2 ">
-                                <p class="text-lg text-black font-bold">准入设置</p>
-                                <div class="Close" @click="accessSettingsVisible3 = false">
-                                    <el-icon size="20" class="cursor-pointer">
-                                        <Close />
-                                    </el-icon>
-                                </div>
-                            </div>
+                        <PipelineAccessSettings />
 
-                            <div class="flex justify-start items-center gap-3 mt-5">
-                                <p>设置帮助</p>
-                                <el-popover :visible="accessSettingsHelpVisible3" placement="right" :width="650">
-                                    <p>自动：流水线执行时自动进入该阶段继续运行。</p>
-                                    <p>手动：流水线执行时需手动确认后才能进入该阶段继续运行。</p>
-                                    <p>时间窗：流水线阶段任务在执行窗口内时可以运行，若未在执行窗口内则无法运行该阶段任务。</p>
-                                    <template #reference>
-                                        <i class="fa-light fa-circle-question"
-                                            @click="accessSettingsHelpVisible3 = !accessSettingsHelpVisible3"></i>
-                                    </template>
-                                </el-popover>
-
-                            </div>
-                            <div class="mt-5">
-                                <el-checkbox v-model="accessSettings1" label="自动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings2" label="手动" class="el-checkbox-class" />
-                                <el-checkbox v-model="accessSettings3" label="时间窗" class="el-checkbox-class" />
-                            </div>
-
-
-                            <div class="flex justify-center items-center gap-5 mt-10">
-                                <div class="bg-black cursor-pointer rounded-lg px-10 py-2"
-                                    @click="accessSettingsVisible3 = false">
-                                    <p class=" text-white">确定</p>
-                                </div>
-
-                                <div class="bg-white cursor-pointer rounded-lg border px-10 py-2"
-                                    @click="accessSettingsVisible3 = false">
-                                    <p class=" text-black">取消</p>
-                                </div>
-                            </div>
-                            <template #reference>
-                                <div class="w-7 h-7 absolute -left-14 top-8 bg-black rounded-full z-50 flex justify-center items-center"
-                                    @click="showAccessSettings(3)">
-                                    <i class="fa-light fa-up-to-bracket fa-rotate-90 " style="color: white;"></i>
-                                </div>
-                            </template>
-                        </el-popover>
                         <div class="-translate-x-5">
                             <div
                                 class="relative flex flex-col bg-white p-3 border-4 border-transparent cursor-pointer rounded-xl shadow-lg hover:border-gray-300">
@@ -534,17 +350,6 @@ const settingVisible2 = ref(false);
 const settingVisible3 = ref(false);
 const settingVisible4 = ref(false);
 
-const accessSettingsVisible1 = ref(false);
-const accessSettingsVisible2 = ref(false);
-const accessSettingsVisible3 = ref(false);
-
-const accessSettingsHelpVisible1 = ref(false);
-const accessSettingsHelpVisible2 = ref(false);
-const accessSettingsHelpVisible3 = ref(false);
-
-const accessSettings1 = ref(false);
-const accessSettings2 = ref(false);
-const accessSettings3 = ref(false);
 
 onMounted(() => {
     const innerWidth = innerRef.value?.scrollWidth || 0
@@ -581,22 +386,7 @@ const showSetting = (index: number) => {
             break;
     }
 }
-const showAccessSettings = (index: number) => {
-    console.log('showAccessSettings');
-    switch (index) {
-        case 1:
-            accessSettingsVisible1.value = true;
-            break;
-        case 2:
-            accessSettingsVisible2.value = true;
-            break;
-        case 3:
-            accessSettingsVisible3.value = true;
-            break;
-        default:
-            break;
-    }
-}
+
 
 
 const updateSettingVisible1 = (newValue: boolean) => {
@@ -639,38 +429,7 @@ const updateSettingVisible4 = (newValue: boolean) => {
     background-image: linear-gradient(90deg, #2a3c85, #2a3c85);
 }
 
-:deep(.el-checkbox__inner) {
-    border-radius: 50%;
-    zoom: 100%;
-}
 
-:deep(.el-checkbox__inner) {
-    width: 19px;
-    height: 19px;
-    border-radius: 22.5px;
-}
-
-:deep(.el-checkbox__inner::after) {
-    border: 1px solid #fff;
-    border-left: 0;
-    border-top: 0;
-    left: 7px;
-    top: 4px;
-}
-
-:deep(.el-checkbox__input.is-checked .el-checkbox__inner::after) {
-    transform: rotate(50deg) scaleY(1.5);
-}
-
-:deep(.el-checkbox__inner:hover) {
-    border-color: var(--text-100);
-}
-
-.el-checkbox-class {
-    --el-checkbox-checked-bg-color: #000;
-    --el-checkbox-checked-input-border-color: #000;
-    --el-checkbox-checked-text-color: #000
-}
 
 .container {
     position: relative;
